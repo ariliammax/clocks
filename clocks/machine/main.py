cb02761cd30142ba0566976166e8d89a64eb68f0
Max Snyder <maxsnyder2000@gmail.com>
2023-03-02 20:47:54 -0500

    skeleton

--- clocks/machine/main.py
+++ clocks/machine/main.py
@@ -1,2 +1,16 @@
 # main.py
 # in clocks.machine
+
+from random import randint
+from time import sleep
+
+
+def start():
+    r = randint(1, 6)
+    while True:
+        print(r)
+        sleep(1 / r)
+
+
+if __name__ == "__main__":
+    start()
--- clocks/system/main.py
+++ clocks/system/main.py
@@ -1,2 +1,10 @@
 # main.py
 # in clocks.system
+
+from clocks.machine.main import start
+from multiprocessing import Process
+
+
+if __name__ == "__main__":
+    for _ in range(3):
+        Process(target=start).start()
